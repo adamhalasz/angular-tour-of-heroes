5dfb2ea01063314d5ccdbe9b2df8a4c161dc309f
Adam Halasz <mail@adamhalasz.com>
2017-05-27 00:10:38 +0200

    git init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http'; 
+import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component'
+import { DashboardComponent } from './dashboard.component' 
+import { HeroDetailComponent } from './hero-detail.component'
+import { HeroService } from './hero.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
